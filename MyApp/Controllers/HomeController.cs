86cefa27f301b0c8830f86822fcc279a76995a0e
janikn089 <janik.n089@gmail.com>
2025-03-17 09:55:55 +0100

    n

--- MyApp/Controllers/HomeController.cs
+++ MyApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MyApp.Data;
 using MyApp.Models;
 using System.Diagnostics;
 
@@ -28,5 +29,36 @@ namespace MyApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    }
+}
+
+public class HomeController : Controller
+{
+    private readonly MyAppContext _db;
+
+    public HomeController(MyAppContext db)
+    {
+        _db = db;
+    }
+
+    // Your existing action methods...
+
+    public IActionResult ProductCategoryChart()
+    {
+        // Get data from your database
+        var categories = _db.Categories.ToList();
+        var products = _db.Items.ToList();
+
+        // Count products in each category
+        var categoryProductCount = categories.Select(c => new {
+            CategoryName = c.Name,
+            ProductCount = products.Count(p => p.CategoryId == c.Id)
+        }).ToList();
+
+        // Pass data to the view
+        ViewBag.CategoryNames = categoryProductCount.Select(c => c.CategoryName).ToList();
+        ViewBag.ProductCounts = categoryProductCount.Select(c => c.ProductCount).ToList();
+
+        return View();
     }
 }
